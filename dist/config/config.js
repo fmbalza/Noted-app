8072129f0d3d2a0efbac727e25f711f87edd1a51
Francisco Javier Mendoza Balza <fmbalza@gmail.com>
2023-10-02 19:20:56 -0400

    BACKEND LOGIN 02/10/2023

---
+++ dist/config/config.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    jwtSecret: process.env.JWT_SECRET || 'somesecrettoken',
+    DB: {
+        URI: process.env.MONGODB_URI || 'mongodb+srv://admin:1234567890@cluster0.bgkv4c6.mongodb.net/?retryWrites=true&w=majority',
+        USER: process.env.MONGODB_USER,
+        PASSWORD: process.env.MONGODB_PASSWORD
+    }
+};
